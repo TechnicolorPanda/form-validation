3cc17e7a564de6e79b3c61e18bc374e7ce56f363
LorriAnn <LorriAnn7@att.net>
2020-12-28 19:20:02 -0500

    display error upon submission if fields are incorrect

--- form.js
+++ form.js
@@ -1,6 +1,13 @@
+// displays error when incorrect information is displayed
+
+function submitError() {
+  const finalError = document.getElementById('submit_error');
+  finalError.innerHTML = 'Please correct incomplete fields';
+}
+
 // creates error when an incorrect email addressed is typed
 
-function emailError(emailInput, emailTest) {
+function emailError(emailInput, emailTest, submit) {
   const error = document.getElementById('email_error');
   if (emailTest) {
     emailInput.className = 'valid';
@@ -8,6 +15,9 @@ function emailError(emailInput, emailTest) {
   } else {
     emailInput.className = 'invalid';
     error.innerHTML = 'Please enter a valid e-mail address';
+    if (submit === true) {
+      submitError();
+    }
   }
 }
 
@@ -17,11 +27,7 @@ function validateEmail(submit) {
   const emailInput = document.getElementById('email').value;
   const validEmail = (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
   const emailTest = validEmail.test(emailInput);
-  if (submit === false) {
-    emailError(emailInput, emailTest);
-  } else {
-    submitError();
-  }
+  emailError(emailInput, emailTest, submit);
 }
 
 // reviews email for validity when typed
@@ -39,7 +45,7 @@ function getEmail() {
 
 // displays error upon incorrect input
 
-function countryError(countryInput, countryTest) {
+function countryError(countryInput, countryTest, submit) {
   const error = document.getElementById('country_error');
   if (countryTest) {
     countryInput.className = 'valid';
@@ -47,6 +53,9 @@ function countryError(countryInput, countryTest) {
   } else {
     countryInput.className = 'invalid';
     error.innerHTML = 'Please enter a country name';
+    if (submit === true) {
+      submitError();
+    }
   }
 }
 
@@ -55,17 +64,16 @@ function countryError(countryInput, countryTest) {
 function validateCountry(submit) {
   const countryInput = document.getElementById('country').value;
   const error = document.getElementById('country_error');
-  if (submit === false) {
-    if (countryInput.length >= 2 && country.length <= 74) {
-      const validCountry = (/^[a-zA-Z0-9.&'_`-]*$/);
-      const countryTest = validCountry.test(country); 
-      countryError(countryInput, countryTest)
-    } else {
-      countryInput.className = 'invalid';
-      error.innerHTML = 'Please enter a name between 2 and 74 characters';
-    }
+  if (countryInput.length >= 2 && countryInput.length <= 74) {
+    const validCountry = (/^[a-zA-Z0-9.&'_`-]*$/);
+    const countryTest = validCountry.test(countryInput);
+    countryError(countryInput, countryTest, submit);
   } else {
-    submitError();
+    countryInput.className = 'invalid';
+    error.innerHTML = 'Please enter a name between 2 and 74 characters';
+    if (submit === true) {
+      submitError();
+    }
   }
 }
 
@@ -82,43 +90,56 @@ function getCountry() {
   });
 }
 
-function getZipCode() {
-  const zipCode = document.getElementById('zipcode');
-  zipCode.addEventListener('input', () => {
-    validateZipCode();
-  });
+// registers error for invalid zip code
+
+function zipCodeError(zipCodeTest, zipCode, submit) {
+  const error = document.getElementById('zipcode_error');
+  if (zipCodeTest) {
+    zipCode.className = 'valid';
+    error.innerHTML = '';
+  } else {
+    zipCode.className = 'invalid';
+    error.innerHTML = 'Please enter a valid numeric zipcode.';
+    if (submit === true) {
+      submitError();
+    }
+  }
 }
 
-function validateZipCode() {
+// defines test for a valid zip code
+
+function validateZipCode(submit) {
   const zipCode = document.getElementById('zipcode').value;
   const error = document.getElementById('zipcode_error');
   if (zipCode.length >= 5 && zipCode.length <= 10) {
     const validZipCode = (/^[0-9-]*$/);
     const zipCodeTest = validZipCode.test(zipCode);
-    if (zipCodeTest) {
-      zipCode.className = 'valid';
-      error.innerHTML = '';
-    } else {
-      zipCode.className = 'invalid';
-      error.innerHTML = 'Please enter a valid numeric zipcode.';
-    }
+    zipCodeError(zipCodeTest, zipCode, submit);
   } else {
     zipCode.className = 'invalid';
-    error.innerHTML = 'Please enter a zip code between 5 and 10 characters.'
+    error.innerHTML = 'Please enter a zip code between 5 and 10 characters.';
+    if (submit === true) {
+      submitError();
+    }
   }
 }
 
-function getPassword() {
-  const password = document.getElementById('password');
-  password.addEventListener('input', () => {
-    validatePassword();
+// registers zip code upon input
+
+function getZipCode() {
+  const zipCode = document.getElementById('zipcode');
+
+  // designates that form has not yet been submitted when printing an error
+  const submit = false;
+
+  zipCode.addEventListener('input', () => {
+    validateZipCode(submit);
   });
 }
 
-function validatePassword() {
-  const password = document.getElementById('password').value;
-  const validPassword = (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]*$/);
-  const passwordTest = validPassword.test(password);
+// registers an error for incorrect password
+
+function passwordError(passwordTest, password, submit) {
   const error = document.getElementById('password_error');
   if (passwordTest) {
     password.className = 'valid';
@@ -126,19 +147,35 @@ function validatePassword() {
   } else {
     password.className = 'invalid';
     error.innerHTML = 'Please enter a valid password';
+    if (submit === true) {
+      submitError();
+    }
   }
-  getConfirm(password);
 }
 
-function getConfirm(password) {
-  const confirm = document.getElementById('confirm');
-  confirm.addEventListener('input', () => {
-    validateConfirm(password);
+// tests password input for validity
+
+function validatePassword(submit) {
+  const password = document.getElementById('password').value;
+  const validPassword = (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]*$/);
+  const passwordTest = validPassword.test(password);
+  passwordError(passwordTest, password, submit);
+}
+
+// registers input for password
+
+function getPassword() {
+  const password = document.getElementById('password');
+
+  // designates that form has not yet been submitted when printing an error
+  const submit = false;
+
+  password.addEventListener('input', () => {
+    validatePassword(submit);
   });
 }
 
-function validateConfirm(password) {
-  const confirm = document.getElementById('confirm').value;
+function confirmError(confirm, password, submit) {
   const error = document.getElementById('confirm_error');
   if (confirm === password) {
     confirm.className = 'valid';
@@ -146,34 +183,60 @@ function validateConfirm(password) {
   } else {
     confirm.className = 'invalid';
     error.innerHTML = 'Passwords must match exactly.';
+    if (submit === true) {
+      submitError();
+    }
   }
 }
 
-// TODO: correct erasure of error messages upon submission
+// validate passwords match
+
+function validateConfirm(submit) {
+  const confirm = document.getElementById('confirm').value;
+  const password = document.getElementById('password').value;
+  confirmError(confirm, password, submit);
+}
+
+// retrieves password confirmation
+
+function getConfirm() {
+  const confirm = document.getElementById('confirm');
+
+  // designates that form has not yet been submitted when printing an error
+  const submit = false;
+
+  confirm.addEventListener('input', () => {
+    validateConfirm(submit);
+  });
+}
+
+// validates all fields upon submission
+
+function submitForm() {
+  const submit = true;
+  validateEmail(submit);
+  validateCountry(submit);
+  validateZipCode(submit);
+  validatePassword(submit);
+  validateConfirm(submit);
+}
+
+// selects submit form
 
 function selectSubmit() {
-  console.log('select submit');
   const submitButton = document.getElementById('submit');
   submitButton.addEventListener('click', () => {
     submitForm();
   });
 }
 
-function submitForm() {
-  const submit = true;
-  validateEmail(submit);
-  validateCountry(submit);
-}
-
-function submitError() {
-  const error = document.getElementById('submit_error');
-  error.innerHTML = 'Please correct incomplete fields';
-}
+// activates fields in the form
 
 (function initializeValidation() {
   getEmail();
   getCountry();
   getZipCode();
   getPassword();
+  getConfirm();
   selectSubmit();
 }());
